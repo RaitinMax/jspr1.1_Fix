8fb8401a30801caf144f250f98d8e3fa2f4820d3
MaxPC <jhosh1212@gmail.com>
2022-09-24 23:33:33 +0700

    Commit

---
+++ src/main/java/ru/netology/Request.java
@@ -0,0 +1,58 @@
+package ru.netology;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Request {
+//par
+    private String method;
+    private String fullpath;
+    private String body;
+    private String path;
+    private String query;
+    private List<NameValuePair> parsedParams;
+    public Request(String method, String fullpath) {
+        this.method = method;
+        this.fullpath = fullpath;
+        if (fullpath.contains("?")) {
+            path = fullpath.substring(0, fullpath.indexOf("?"));
+            query = fullpath.substring(fullpath.indexOf("?") + 1);
+            parsedParams = URLEncodedUtils.parse(query, StandardCharsets.UTF_8, '&');
+        } else {
+            path = fullpath;
+        }
+
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public List<NameValuePair> getQueryList() {
+        return parsedParams;
+    }
+
+    public List<NameValuePair> getQueryParam(String param) {
+        return parsedParams.stream().filter(p -> p.getName().equals(param)).collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/ru/netology/Server.java
@@ -0,0 +1,120 @@
+package ru.netology;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.CharBuffer;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+public class Server {
+    private static final int PORT = 9999;
+    final List<String> validPaths = List.of("/spring.svg", "/spring.png", "/legacy/resources.html", "/styles.css",
+            "/app.js", "/classic.html", "/index.html", "/events.js", "/messages", "/urlencode");
+    ExecutorService pool = Executors.newFixedThreadPool(64);
+    private ConcurrentHashMap<String, ConcurrentHashMap<String, IHandler>> byMethod = new ConcurrentHashMap();
+//a
+
+    public void startServer() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        while (true) {
+            var socket = serverSocket.accept();
+            if (socket.isBound()) {
+                pool.submit(() -> process(socket));
+            }
+        }
+    }
+
+    private void process(Socket socket) {
+        while (!socket.isClosed()) {
+            try (
+                    final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    final var out = new BufferedOutputStream(socket.getOutputStream())
+            ) {
+                if (in.ready()) {
+                    String requestLines = parse(in);
+                    String bodyLines = getBody(requestLines);
+
+                    if (!requestLines.isEmpty()) {
+                        final var parts = requestLines.lines().findFirst().get().split(" ");
+                        if (parts.length != 3) {
+                            // just close socket
+                            continue;
+                        }
+                        var method = parts[0];
+                        var path = parts[1];
+                        Request req = new Request(method, path);
+
+                        if (bodyLines != null && !bodyLines.isEmpty()) {
+                            req.setBody(bodyLines);
+                        }
+
+                        IHandler handler = byMethod.get(req.getMethod()).get(req.getPath());
+                        if (handler != null) {
+                            handler.handle(req, out);
+                        } else {
+                            out.write((
+                                    "HTTP/1.1 404 Not Found\r\n" +
+                                            "Content-Length: 0\r\n" +
+                                            "Connection: close\r\n" +
+                                            "\r\n"
+                            ).getBytes());
+                            out.flush();
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void addHandler(String method, String s, IHandler handler) {
+        if (byMethod.containsKey(method)) {
+            byMethod.get(method).put(s, handler);
+        } else {
+            ConcurrentHashMap<String, IHandler> map = new ConcurrentHashMap<>();
+            map.put(s, handler);
+            byMethod.put(method, map);
+        }
+    }
+
+    private String parse(BufferedReader in) throws IOException {
+        CharBuffer reqBuffer = CharBuffer.allocate(1024);
+        int count = in.read(reqBuffer);
+        if (count != -1) {
+            reqBuffer.clear();
+            return new String(reqBuffer.array(), 0, count);
+        }
+        reqBuffer.clear();
+        return null;
+    }
+
+    private String getBody(String requestLines) {
+        List<String> reqLines = requestLines.lines().collect(Collectors.toList());
+        int bodyStart = 0;
+        StringBuilder body = new StringBuilder();
+        for (int i = 0; i < reqLines.size(); i++) {
+            if (reqLines.get(i).isEmpty()) {
+                bodyStart = i + 1;
+                break;
+            }
+        }
+        if (bodyStart < reqLines.size() && bodyStart != 0) {
+            for (int i = bodyStart; i < reqLines.size(); i++) {
+                body.append(reqLines.get(i).trim());
+            }
+        }
+        return body.toString();
+    }
+}
+
+
+
